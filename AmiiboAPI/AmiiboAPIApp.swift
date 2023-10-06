09cd998f62df687d74da5a3ff4bcad84be3ea7af
Joel Lacerda <joellacerdaol@gmail.com>
2023-10-06 15:58:40 -0300

    Initial Commit

---
+++ AmiiboAPI/AmiiboAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmiiboAPIApp.swift
+//  AmiiboAPI
+//
+//  Created by Joel Lacerda on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AmiiboAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
